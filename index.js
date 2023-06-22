760377cc0f47651337c4795e857975db8dcf0466
Nene <133941254+NeneShallCode@users.noreply.github.com>
2023-06-21 20:03:28 -0700

    added icon

--- index.js
+++ index.js
@@ -17,16 +17,20 @@ function showTemp(response){
   let weatherEl = document.querySelector("#describe");
   let humidEl = document.querySelector("#humidity");
   let windEl = document.querySelector("#wind");
-  let dateEl = document.querySelector("#time")
+  let dateEl = document.querySelector("#time");
+  let iconEl=document.querySelector("#icon")
+
   tempEl.innerHTML = Math.round(response.data.main.temp);
   cityEl.innerHTML = response.data.name;
   weatherEl.innerHTML = response.data.weather[0].description;
   humidEl.innerHTML = response.data.main.humidity;
   windEl.innerHTML = Math.round(response.data.wind.speed);
   dateEl.innerHTML = formatDate(response.data.dt * 1000)
+  iconEl.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
 }
+city="paris"
 
 let apiKey="514a1ffade9078bc9c2d40d114f61a0b";
-let apiUrl=`https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${apiKey}&units=metric`;
+let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
 axios.get(apiUrl).then(showTemp);
