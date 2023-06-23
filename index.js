cb34253f381e04d49916486a4ef8cfb7125cff58
Nene <133941254+NeneShallCode@users.noreply.github.com>
2023-06-23 13:30:00 -0700

    added farenheit/celsius converter

--- index.js
+++ index.js
@@ -18,19 +18,52 @@ function showTemp(response){
   let humidEl = document.querySelector("#humidity");
   let windEl = document.querySelector("#wind");
   let dateEl = document.querySelector("#time");
-  let iconEl=document.querySelector("#icon")
+  let iconEl=document.querySelector("#icon");
 
-  tempEl.innerHTML = Math.round(response.data.main.temp);
+  celciusTemp = response.data.main.temp
+
+  tempEl.innerHTML = Math.round(celciusTemp);
   cityEl.innerHTML = response.data.name;
   weatherEl.innerHTML = response.data.weather[0].description;
   humidEl.innerHTML = response.data.main.humidity;
   windEl.innerHTML = Math.round(response.data.wind.speed);
-  dateEl.innerHTML = formatDate(response.data.dt * 1000)
-  iconEl.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
+  dateEl.innerHTML = formatDate(response.data.dt * 1000);
+  iconEl.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 }
-city="paris"
 
-let apiKey="514a1ffade9078bc9c2d40d114f61a0b";
-let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+function search(city){
+  let apiKey="514a1ffade9078bc9c2d40d114f61a0b";
+  let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  axios.get(apiUrl).then(showTemp);
+}
 
-axios.get(apiUrl).then(showTemp);
+function handleSubmit(event){
+    event.preventDefault();
+    let cityInputEl = document.querySelector("#city-input");
+    search(cityInputEl.value)
+}
+
+function showFarenheit(event){
+    event.preventDefault()
+      let tempEl = document.querySelector("#temp");
+    let farenheitTemp = celciusTemp
+    tempEl.innerHTML = Math.round(farenheitTemp)
+}
+
+function showCelsius(event){
+    event.preventDefault()
+    let tempEl = document.querySelector("#temp");
+    let celciusTem = (celciusTemp*9)/5+32;
+    tempEl.innerHTML = Math.round(celciusTem)
+}
+
+let celciusTemp = null;
+
+let form = document.querySelector("#city-search")
+form.addEventListener("submit", handleSubmit)
+
+let farenheitLink = document.querySelector("#farenheit-link")
+farenheitLink.addEventListener("click", showFarenheit)
+
+let celsiusLink = document.querySelector("#celsius-link")
+celsiusLink.addEventListener("click", showCelsius)
