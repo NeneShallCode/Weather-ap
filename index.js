c629a2a40ba4b28fdc42031f25f06fb8711d8f85
Nene <133941254+NeneShallCode@users.noreply.github.com>
2023-06-26 10:49:32 -0700

    fought and won² 💪

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ function formatDate(timestamp){
  return `${day}, ${hours}:${min}`
 }
 
+function formatDay(timestamp){
+  let date = new Date(timestamp * 1000)
+  let day = day.getDay();
+  let days = ["Mon","Tue","Wed","Thu","Fri"]
+  return day;
+}
+
 function displayForecast(response) {
   console.log(response.data)
   let forecast = response.data.daily;
@@ -23,6 +30,7 @@ function displayForecast(response) {
         forecastHTML +
         `
       <div class="col-2">
+        <div class = "days">${formatDay(forecastDay.dt)}</div>
         <img
           src="http://openweathermap.org/img/wn/${
             forecastDay.weather[0].icon
@@ -30,11 +38,11 @@ function displayForecast(response) {
           alt=""
           width="42"
         />
-        <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> ${Math.round(
+        <div class="weather-fore-temp">
+          <span class="weather-fore-max"> ${Math.round(
             forecastDay.temp.max
           )}° </span>
-          <span class="weather-forecast-temperature-min"> ${Math.round(
+          <span class="weather-fore-min"> ${Math.round(
             forecastDay.temp.min
           )}° </span>
         </div>
